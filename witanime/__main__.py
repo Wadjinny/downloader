2f5c1a153cecea884d0590d980b0a94b16858c1a
ilyas <ilyasswadjinny@gmail.com>
2023-08-15 06:53:28 +0200

    bug fix

--- witanime/__main__.py
+++ witanime/__main__.py
@@ -100,11 +100,15 @@ def get_drive_from_yonaplay(yonaplay_link):
 
     response = response.text
     # https://drive.google.com/file/d/1Pk56A0la0_SufhDDxeOwltT9k4y7cbPw/preview
-    drive_id = re.search(r"https://drive.google.com/file/d/(.*?)/preview", response).group(1)
+    drive_id = re.search(r"https://drive.google.com/file/d/(.*?)/preview", response)
+    if drive_id:
+        drive_id = drive_id.group(1)
+    else:
+        print("Error getting drive id from yonaplay")
     # print(f"{drive_id = }")
-    return drive_id
+    return None
 
-# %% id 
+# %% drive id 
 def get_drive_id_from_episode(episode_link):
     payload={}
     headers = {
@@ -137,41 +141,6 @@ def get_drive_id_from_episode(episode_link):
     # print(f"{drive_id = }")
     return drive_id
     
-#%% mediafire
-# pip3 install git+https://github.com/Juvenal-Yescas/mediafire-dl
-def get_mediafire_from_episode(episode_link):
-    payload={}
-    headers = {
-    'authority': 'witanime.org',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'no-cache',
-    'cookie': '_ga=GA1.1.2071926742.1692061864; cf_clearance=XfaGiHbqrdN5zPchSNvJB6QhxyTKEnRdzYm9dGJnOCI-1692061865-0-1-4e523320.202ebdf0.119b4fa-160.2.1692061865; _ga_8MM0QDL0KJ=GS1.1.1692061863.1.1.1692062159.0.0.0',
-    'pragma': 'no-cache',
-    'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Linux"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
-    }
-
-    response = requests.request("GET", episode_link, headers=headers, data=payload)
-
-    response = response.text
-    # https://www.mediafire.com/file/3vtjsfemuw5arv4/%5BWitanime.com%5D_RZKHISHNK_BD-FHD.mp4/file
-    mediafire_link = re.search(r"https://www.mediafire.com/file/.*/.*/file", response)
-    if mediafire_link:
-        mediafire_link = mediafire_link.group()
-    else:
-        yonaplay_id = re.search(r"https://yonaplay.org/embed.php\?id=\d+", response).group()
-        drive_id = get_drive_from_yonaplay(yonaplay_id)
-    # print(f"{drive_id = }")
-    return drive_id
-
 # %%
 def main():
     # search_term = "no game no life"
