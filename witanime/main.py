284d35ad16baadce274d29f09c7ff33f2401e899
ilyas wadjinny <70728324+Wadjinny@users.noreply.github.com>
2023-08-16 19:57:25 +0000

    added structure

--- witanime/main.py
+++ witanime/main.py
@@ -1,86 +1,10 @@
 #%% search
-import requests
-from bs4 import BeautifulSoup as bs
-import re
-import gdown 
 from pathlib import Path
-#to url encode
-from urllib.parse import quote
+import gdown 
 from witanime.video_providers.drive import get_drive_ids_from_episode
 from witanime.video_providers.mediafire import get_mediafire_links_from_episode
-
-
+from witanime.anime_site.witanime import get_episodes_list, get_search_results_link
 import argparse
-
-
-#%% search
-def get_search_results_link(search_term):
-    search_term = quote(search_term)
-    url = f"https://witanime.org/?search_param=animes&s={search_term}"
-
-    payload={}
-    headers = {
-    'authority': 'witanime.org',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'no-cache',
-    'cookie': '_ga=GA1.1.2071926742.1692061864; cf_clearance=XfaGiHbqrdN5zPchSNvJB6QhxyTKEnRdzYm9dGJnOCI-1692061865-0-1-4e523320.202ebdf0.119b4fa-160.2.1692061865; _ga_8MM0QDL0KJ=GS1.1.1692061863.1.1.1692062159.0.0.0',
-    'pragma': 'no-cache',
-    'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Linux"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
-    }
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-
-    response = response.text
-    soup = bs(response, 'html.parser')
-    # .hover.ehover6>.overlay
-    links = soup.select('.hover.ehover6>.overlay')
-    links = [link['href'] for link in links]
-    # print(f"{len(links) = }")
-    return links
-
-
-
-# %% epsides list
-def get_episodes_list(anime_link):
-    payload={}
-    headers = {
-    'authority': 'witanime.org',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'no-cache',
-    'cookie': '_ga=GA1.1.2071926742.1692061864; cf_clearance=XfaGiHbqrdN5zPchSNvJB6QhxyTKEnRdzYm9dGJnOCI-1692061865-0-1-4e523320.202ebdf0.119b4fa-160.2.1692061865; _ga_8MM0QDL0KJ=GS1.1.1692061863.1.1.1692062159.0.0.0',
-    'pragma': 'no-cache',
-    'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Linux"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
-    }
-
-    response = requests.request("GET", anime_link, headers=headers, data=payload)
-
-    response = response.text
-    soup = bs(response, 'html.parser')
-    # .hover.ehover6>.overlay
-    episodes = soup.select('.hover.ehover6>.overlay')
-    episodes = [episode['href'] for episode in episodes]
-    episodes_size = len(episodes)
-    # print(f"{episodes_size = }")
-    # print(f"{episodes[:10] = }")
-    return episodes
 
 def download_from_mediafire(episode_link, output_path):
     import mediafire_dl
